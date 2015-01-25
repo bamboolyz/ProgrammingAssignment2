cf238e4a4fe92f644f59a20e784fde5d8183cf21
bamboolyz <bamboo_adaliu@yahoo.com>
2015-01-25 21:56:18 +0800

    2nd draft input explanation

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix has two variables, x and m 
+## x is a matrix vector
+## m is used to store the value after the processing
 
 makeCacheMatrix <- function(x = matrix()) {
     m<- NULL
@@ -19,9 +18,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## the variable x input-ed must be a makeCacheMatrix 
 
 cacheSolve <- function(x, ...) {
+## x return a makeCacheMatrix
+## x$getinv uses the makeCacheMatrix vector's getinv function, confirm if the getinv is calculated before
     m<-x$getinv()
     if(!is.null(m)){
             message("getting cached data")
